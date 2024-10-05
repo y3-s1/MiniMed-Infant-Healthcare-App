ab453a942152ead3f298149cef477af8ac75524e
thilanRandika <dgtrandika@gmail.com>
2024-10-05 12:54:41 +0530

    implement tab navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#2896B5' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="vaccination"
+        options={{
+          title: 'Vaccination',
+          tabBarIcon: ({ color }) => <MaterialIcons name="vaccines" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="appointments"
+        options={{
+          title: 'Appointments',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="tasks" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="events"
+        options={{
+          title: 'Events',
+          tabBarIcon: ({ color }) => <Feather name="calendar" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
